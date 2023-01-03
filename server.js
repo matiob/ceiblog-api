3fc511a9d5a63bca3c7c60227e86b4148db90b2b
matiob <buraschi.mateo@gmail.com>
2023-01-03 01:20:10 -0300

    cors

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const morgan = require("morgan");
-//const cors = require("cors");
+const cors = require("cors");
 require("./config"); // --> require the db()
 const router = require("./routes");
 
-/* app.use(
+app.use(
   cors({
-    origin: `http://localhost:${process.env.PORT}/`,
+    origin: `https://ceiblog.netlify.app/`,
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
-); */
+);
 
 app.use(express.json());
 app.use(morgan("tiny"));
