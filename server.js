016043aa3be0437bd4bd197cb10b0604ea87ae68
matiob <buraschi.mateo@gmail.com>
2023-01-06 12:36:18 -0300

    multer lib

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 require("./config"); // --> require the db()
 const router = require("./routes");
+const multer = require("multer");
 
 app.use(
   cors({
@@ -16,6 +17,21 @@ app.use(
 
 app.use(express.json());
 app.use(morgan("tiny"));
+
+//----save image----//
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use("/api", router);
 
